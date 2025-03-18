bf63fb80d8edf823839bb38682ac77a76857148d
GitHub Action <action@github.com>
2025-03-18 10:00:25 +0000

    Template cleanup

--- src/test/kotlin/com/github/sanderpostma/ijgitbranchtitle/MyPluginTest.kt
+++ src/test/kotlin/com/github/sanderpostma/ijgitbranchtitle/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.sanderpostma.ijgitbranchtitle
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.openapi.components.service
@@ -6,7 +6,7 @@ import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.sanderpostma.ijgitbranchtitle.services.MyProjectService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
